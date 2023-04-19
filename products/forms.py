e62ef1a0278b6825162130cb36ee8e23d241f258
adamsburge <adam.scott.burge@gmail.com>
2023-04-19 08:21:25 +0000

    finish adjusting the add image button

--- products/forms.py
+++ products/forms.py
@@ -4,7 +4,6 @@ from .models import Product, Category
 
 
 class ProductForm(forms.ModelForm):
-
     class Meta:
         model = Product
         fields = '__all__'
@@ -15,7 +14,6 @@ class ProductForm(forms.ModelForm):
         super().__init__(*args, **kwargs)
         categories = Category.objects.all()
         friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
-
         self.fields['category'].choices = friendly_names
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'border-black rounded-0'
